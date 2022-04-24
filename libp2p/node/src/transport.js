edbea94b209989276085465ee8cc3c86c3831579
David Djekic <david.djekic@exlrt.com>
2022-04-24 23:37:19 +0200

    Created three nodes and used KadDHT to connect them.

--- libp2p/node/src/transport.js
+++ libp2p/node/src/transport.js
@@ -2,63 +2,67 @@ const Libp2p = require('libp2p')
 const TCP = require('libp2p-tcp')
 const { NOISE } = require('libp2p-noise')
 const MPLEX = require('libp2p-mplex')
-const WebSockets = require('libp2p-websockets')
+const KadDHT = require('libp2p-kad-dht')
+const delay = require('delay')
+const Gossipsub = require('libp2p-gossipsub')
 
-const createNode = async (transports, addresses = []) => {
-    if (!Array.isArray(addresses)) {
-        addresses = [addresses]
-      }
-    
+
+const createNode = async () => {
   const node = await Libp2p.create({
     addresses: {
-      listen: addresses
+      listen: ['/ip4/0.0.0.0/tcp/0']
     },
     modules: {
-      transport: transports,
+      transport: [TCP],
       connEncryption: [NOISE],
-      streamMuxer: [MPLEX]
+      streamMuxer: [MPLEX],
+      dht: KadDHT,
+      pubsub: Gossipsub
+    },
+    config: {
+      dht: {
+        enabled: true
+      }
     }
   })
 
-  await node.start()
   return node
 }
 
-function printAddrs (node, number) {
-    console.log('node %s is listening on:', number)
-    node.multiaddrs.forEach((ma) => console.log(ma.toString()))
-  }
-
-function print ({ stream }) {
-    console.log('stream: ', stream)
-}
 
 ;(async () => {
+  const topic = 'nodes_topic'
     const [node1, node2, node3] = await Promise.all([
-        createNode([TCP], '/ip4/0.0.0.0/tcp/0'),
-        createNode([TCP,WebSockets], ['/ip4/0.0.0.0/tcp/0', '/ip4/127.0.0.1/tcp/10000/ws']),
-        createNode([WebSockets], '/ip4/127.0.0.1/tcp/20000/ws')
+        createNode(),
+        createNode(),
+        createNode()
       ])
     
-      printAddrs(node1, '1')
-      printAddrs(node2, '2')
-      printAddrs(node3, '3')
-
-      node1.handle('/print', print)
-      node2.handle('/print', print)
-      node3.handle('/print', print)
+    await node1.start()
+    await node2.start()
+    await node3.start()
 
     await node1.peerStore.addressBook.set(node2.peerId, node2.multiaddrs)
     await node2.peerStore.addressBook.set(node3.peerId, node3.multiaddrs)
-    await node3.peerStore.addressBook.set(node1.peerId, node1.multiaddrs)
 
-    await node1.dialProtocol(node2.peerId, '/print')
-    await node2.dialProtocol(node3.peerId, '/print')
+    await Promise.all([
+      node1.dial(node2.peerId),
+      node2.dial(node3.peerId)
+    ])
+
+    await delay(100)
+    await node1.peerRouting.findPeer(node3.peerId)
     
-    try {
-        await node3.dialProtocol(node1.peerId, '/print')
-      } catch (err) {
-        console.log('node 3 failed to dial to node 1 with:', err.message)
-      }
+    node3.pubsub.subscribe(topic)
+    node3.pubsub.on(topic, (message) => {
+      let str = Buffer.from(message.data).toString();
+      console.log(`node3 received: ${str}`)
+      
+    })
 
+
+    setInterval(() => {
+      let uint8arr = new Uint8Array(Buffer.from("david"));
+      node1.pubsub.publish(topic, uint8arr)
+    }, 1000)
   })();
